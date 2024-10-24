ff775a72cc516672e474b0e227318f2303a62425
elenasosnina <tatalenakot@gmail.com>
2024-10-24 21:20:48 +0300

    добавление проекта

---
+++ lib/Pages/MenuPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using lib.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lib.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuPage.xaml
+    /// </summary>
+    public partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BooksPage booksPage = new BooksPage();
+            MenuWindow menuWindow = (MenuWindow)Window.GetWindow(this);
+            menuWindow.Title = booksPage.Title;
+            menuWindow.MenuFrame.Navigate(booksPage);
+        }
+    }
+}
